8901bd5e0a9627a3ec69119d75f905b1cbc4ea5c
Ashley Stonebridge <ashley.ann.stonebridge@gmail.com>
2021-07-06 23:28:21 -0700

    updated prompt window for length of password to only allow for numbers and first attempt at generating the password with just letters

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ var passwordLength = 0;
 
 //Function to store the input entered by the user for the length of the password
 //Function also ensures that the minimum length of password is greater than or equal to 8
-//NOT WORKING - Function also checks to make sure that the user is only allowed to input a number
 function askLength() {
   passwordLength = prompt("Please input a number for the length of your password.");
-  // if (passwordLength = isNaN()){
-  //   alert("Please input a number.");
-  //   askLength();
-  // }
-  if (passwordLength < 8) {
-    alert("Please choose a number greater than or equal to 8.");
+  if (isNaN(passwordLength) === true) {
+    alert("Please input a number.")
     askLength();
+  } 
+  if (passwordLength < 8) {
+        alert("Please choose a number greater than or equal to 8.");
+        askLength();
   }
 }
 
@@ -26,26 +25,35 @@ var letters = ("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz");
 var lettersCount = 0;
 
 //Function to ask user if they want to include letters in their password
-//NOT WORKING - In the prompt window when the user presses OK, then letters will be included in the password. Otherwise, if pressed Cancel, then letters will not be included
+//If user wants to include letters, then a random number is chosen from 1 to the paswordLength
 function askLetters() {
   includeLetters = prompt("Press OK if you would like to include letters in your password. Otherwise, press Cancel.");
   if (includeLetters !== null) {
-    lettersCount = Math.floor(Math.random(0, passwordLength - 1))
+    lettersCount = Math.floor(Math.random()*passwordLength)+1
   }
 }
 
-//Calls the function to know if letters will be included in the password or not
+//Calls the function to know if letters will be included in the password or not and if so, how many
 askLetters();
 console.log(includeLetters);
 console.log(lettersCount);
 
+function generatePassword () {
+  for (var i=0; i<lettersCount; i++) {
+    console.log(letters[Math.floor(Math.random()*i)])
+  }  
+}
 
-//Below is the starter code included in the homework assignment
+generatePassword();
 
-// Assignment Code
+//STARTER CODE - Below is the starter code included in the homework assignment
+
+//Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -54,6 +62,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Add event listener to generate button
 
+generateBtn.addEventListener("click", writePassword);
