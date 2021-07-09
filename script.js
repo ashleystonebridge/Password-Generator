3cfa1014886c26a38765d77faf2304250ed4f551
Ashley Stonebridge <ashley.ann.stonebridge@gmail.com>
2021-07-08 18:26:45 -0700

    updates

--- script.js
+++ script.js
@@ -16,14 +16,13 @@ function askLength() {
 }
 
 //Assigns arrays to variables for the different types of characters the user can choose to include in their password
-//OPEN TO RESEARCH/UPDATE - CAN I INPUT AS ONE LONG STRING WITHOUT THE QUOTES? IF NOT, UPDATE TO INCLUDE ALL LETTERS, ALL NUMBERS, ETC...
-var lowerCase = ["abcdefg"]
-var upperCase = ["ABCDEFG"]
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 var numbers = ["0123456789"]
 var symbols = ["!@#$%^&*()"]
 
 //OPEN TO DO - ADD COMMENTS HERE TO EXPLAIN THIS FUNCTION
-function generatePassword () {
+function generatePassword() {
   var length = askLength();
   var includeLower = confirm("Use lowercase?");
   var includeUpper = confirm("Use uppercase?");
@@ -32,11 +31,10 @@ function generatePassword () {
   if (includeLower === false &&
     includeUpper === false &&
     includeNumbers === false &&
-    includeSymbols === false){
-      alert ("Please select OK for at least one type of character to include in your password.")
-      //OPEN TO DEBUG - INSTEAD OF RECALLING GENERATEPASSWORD FUNCTION IT GOES BACK TO ASKLENGTH, AND THEN IT KINDA BREAKS BECAUSE WON'T WRITE THE PASSWORD
-      generatePassword();
-    }
+    includeSymbols === false) {
+      alert("Please select OK for at least one type of character to include in your password.")
+      return generatePassword();
+  }
   var pot = "";
   var finalPassword = "";
   
@@ -59,9 +57,8 @@ function generatePassword () {
   console.log(`pot: ${pot}`);
 
   for (let i=0; i<length; i++) {
-    var index = Math.floor(Math.random()*pot.length);
-    var temp = pot[index];
-    finalPassword += temp;
+    var index = Math.floor(Math.random() * pot.length);
+    finalPassword += pot[index];
   }
   
   console.log(finalPassword);
@@ -79,6 +76,6 @@ function writePassword() {
 }
 
 //Selects the Generate Password button, adds a click event listener, so when the button is clicked, the writePassword function is called
-var generateBtn = document.querySelector("#generate");
+var generateButton = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", writePassword);
+generateButton.addEventListener("click", writePassword);
